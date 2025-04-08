4b0429c0b3256d2b3a0638f1290562e625dff618
otmaneaksa <otmaneksaani@gmail.com>
2025-04-08 17:47:14 +0100

    begin at MealPlan component

--- Front/front/src/App.js
+++ Front/front/src/App.js
@@ -6,25 +6,9 @@ import MealPlanner from './Components/MealPlanner';
 import GroceryList from './Components/GroceryList';
 
 function App() {
-  /*const [mealPlan, setMealPlan] = useState([]);
-  const [groceryList, setGroceryList] = useState([]);
-  useEffect(() => {
-    // Fetch meal plan from backend
-    axios.get('/api/meal-plan')
-      .then(response => setMealPlan(response.data))
-      .catch(error => console.error(error));
-  }, []);
-  const generateGroceryList = () => {
-    axios.post('/api/grocery-list', { meals: mealPlan })
-      .then(response => setGroceryList(response.data))
-      .catch(error => console.error(error));
-  };*/
   return (
     <div className="App">
-      <h1>Planificateur de Repas</h1>
       <MealPlanner/>
-      {/*<button onClick={generateGroceryList}>Générer la Liste de Courses</button>*/}
-      <GroceryList/>
     </div>
   );
 }
--- Front/front/src/Components/MealPlanner.js
+++ Front/front/src/Components/MealPlanner.js
@@ -1,24 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import WeaklyPlan from './WeaklyPlan';
+import MealSugg from './MealSugg';
+import NutritionSum from './NutritionSum';
 import axios from 'axios';
 
 const MealPlanner = ({ dietType }) => {
-  const [mealPlan, setMealPlan] = useState([]);
-
-  useEffect(() => {
-    // Fetch meal plan from backend
-    axios.get(`/api/meal-plan?diet=${dietType}`)
-      .then(response => setMealPlan(response.data))
-      .catch(error => console.error(error));
-  }, [dietType]);
 
   return (
     <div>
-      <h2>Plan de Repas</h2>
-      <ul>
-        {mealPlan.map((meal, index) => (
-          <li key={index}>{meal.day}: {meal.meal}</li>
-        ))}
-      </ul>
+       <WeaklyPlan/>
+       <MealSugg/>
+       <NutritionSum/>
     </div>
   );
 };
