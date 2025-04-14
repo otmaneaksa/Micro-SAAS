194d997f8b746b4854655f9659951948c25663bb
otmaneaksa <otmaneksaani@gmail.com>
2025-04-14 17:43:30 +0100

    GroceryList component at style in progress

--- Front/front/src/Components/GroceryList/GroceryList.js
+++ Front/front/src/Components/GroceryList/GroceryList.js
@@ -1,38 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import GrocerySum from './GrocerySum';
+import styled from 'styled-components';
 
 const GroceryList = ({ mealPlan }) => {
-  const [groceryTypes, setGroceryTypes] = useState(['Groceries', 'Snacks', 'Drinks', 'Vegetables', 'Fruits']);
-  const [groceryItems, setGroceryItems] = useState([{type:'Groceries',name:'Almond Milk'},{type:'Groceries',name:'Chicken Breast'},{type:'Vegetables',name:'Apples'},{type:'Vegetables',name:'Bananas'},{type:'Vegetables',name:'Carrots'}]);
+  const [GroceryTypes, setGroceryTypes] = useState(['Groceries', 'Snacks', 'Drinks', 'Vegetables', 'Fruits']);
+  const [NotEmptyTypes, setNotEmptyTypes] = useState([]);
+  const [GroceryItems, setGroceryItems] = useState([{type:'Groceries',name:'Almond Milk'},{type:'Groceries',name:'Chicken Breast'},{type:'Vegetables',name:'Apples'},{type:'Vegetables',name:'Bananas'},{type:'Vegetables',name:'Carrots'}]);
   const [groceryItem, setGroceryItem] = useState({
     type: '',
     name:''
   });
+  useEffect(() => {
+    const NoEmptyTypes = GroceryTypes.filter((type) => {
+      return GroceryItems.some((item) => item.type === type);
+    })
+    setNotEmptyTypes(NoEmptyTypes);
+  },[])
+  const GroceryHeader = styled.header`
+    display: flex;
+    justify-content:space-around;
+    align-items: center;
+  `;
+  const Input = styled.input`
+  padding:5px;
+  border-radius:5px;
+  `;
+  const GroceryContent = styled.div`
+    display: flex;
+    justify-content:space-around;
+    align-items: center;
+  `;
+  const GroceyListContainer = styled.div`
+
+  `;
+  const GroceryItem = styled.div`
+  text-align:left;
+  border:1px solid white;
+  boerder-radius:5px;
+  padding:10px 100px 10px 10px;
+  margin:10px;
+  box-shadow: 4px 8px 10px rgba(0, 0, 0, 0.1);
+  `;
   return (
     <>
-      <div className="grocery-header">
-        <h2>Grocery List</h2>
-        <input type="text" placeholder="search items" className="grocery-search"/>
-      </div>
-      <GrocerySum/>
-      {groceryTypes.map((type, index) => {
+      <GroceryHeader>
+        <h2 style={{display:'inline-block'}}>Grocery List</h2>
+        <Input type="text" placeholder="search items" className="grocery-search"/>
+      </GroceryHeader>
+      <GroceryContent>
+        <GroceyListContainer>
+          {NotEmptyTypes.map((type, index) => {
         return (
-          <div className="grocery-type" key={index}> 
-            <h3>{type}</h3>
-            {groceryItems.map((item, index) => {
-          if(item.type == type){
-            return(
-              <div className="grocery-type--item" key={index}>
-                <input type="checkbox" id={item.name} name={item.name} value={item.name}/>
-                <label for={item.name}>{item.name}</label>
-              </div>
-            )
-          }
-        })}
-          </div>
-          )
-      })}
+              <GroceryItem className="grocery-type" key={index}> 
+                <h3>{type}</h3>
+                {GroceryItems.map((item, index) => {
+                  if(item.type == type){
+                    return(
+                      <div className="grocery-type--item" key={index}>
+                        <input type="checkbox" id={item.name} name={item.name} value={item.name}/>
+                        <label for={item.name}>{item.name}</label>
+                      </div>
+                    )
+                  }
+                })}
+              </GroceryItem>
+              )
+          })}
+        </GroceyListContainer>
+        <GrocerySum/>
+      </GroceryContent>
     </>
   );
 };
--- Front/front/src/Components/GroceryList/GrocerySum.js
+++ Front/front/src/Components/GroceryList/GrocerySum.js
@@ -1,14 +1,22 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
 
 export default function GrocerySum() {
   const [totalItems, setTotalItems] = useState(0);
   const [totalCost, setTotalCost] = useState(0);
+  const GrocerySumContainer = styled.div`
+  text-align:left;
+  border:1px solid white;
+  border-radius:5px;
+  box-shadow: 4px 8px 10px rgba(0, 0, 0, 0.1);
+  padding:5px 50px 50px 5px;
+  `;
   return (
-    <>
+    <GrocerySumContainer>
         <h3>Summary</h3>
         <p>Total Items: {totalItems}</p>
         <p>Estimated Cost: ${totalCost}</p>
         <button>Generate Shopping List</button>
-    </>
+    </GrocerySumContainer>
   )
 }
