8599c516186ee6bc789e24223e3446112ca34b91
otmaneaksa <otmaneksaani@gmail.com>
2025-04-24 14:22:37 +0100

    Page Home in progress

--- Front/front/src/Components/Home.js
+++ Front/front/src/Components/Home.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
-import Regimen from "../assets/images/Regimen.png";
+import Regimen from "../assets/images/Regimen.jpg";
 
 export default function Home() {
   const Welcome = styled.div`
   `;
   const FeaturedMealPlans = styled.div`
-
+    img{
+      width: 300px;
+      height: auto;
+      border-radius:10px;
+    }
+      div.meal-plans-container {
+        text-align:left;
+        display: flex;
+        justify-content: space-around;
+        margin-bottom:20px;
+        }
+      div.meal-plan-card{
+        margin-right:20px;
+        padding:15px;
+        border-radius:5px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+      }
+  `;
+  const LatestRecipes = styled.div`
+    img{
+      width: 200px;
+      height: auto;
+      border-radius:10px;
+    }
+    display: flex;
+    justify-content: space-around;
+    background-color:#DDE6ED;
+    padding:15px 0;
+    div.meal-plan-card{
+      background-color:white;
+      padding:15px;
+      border-radius:5px;
+    };
   `;
   return (
     <>
@@ -17,7 +49,7 @@ export default function Home() {
       </Welcome>
       <FeaturedMealPlans>
         <h2>Featured Meal Plans</h2>
-        <div>
+        <div className="meal-plans-container">
           <div className="meal-plan-card">
             <img src={Regimen} alt="Meal Plan" />
             <h3>Fresh & Healthy</h3>
@@ -37,7 +69,8 @@ export default function Home() {
             <button>View Details</button>
           </div>
         </div>
-        <div className="latest-recipes">
+      </FeaturedMealPlans>
+      <LatestRecipes>
           <div className="meal-plan-card">
             <img src={Regimen} alt="Meal Plan" />
             <h3>Fresh & Healthy</h3>
@@ -58,8 +91,7 @@ export default function Home() {
             <h3>Breakfast Boost</h3>
             <p>Road recipe</p>
           </div>
-        </div>
-      </FeaturedMealPlans>
+        </LatestRecipes>
     </>
   )
 }
